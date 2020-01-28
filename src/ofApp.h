1cdcd29986106749a52fa60e02b3fa5356bc4e2b
santa-sukitoku <santanaruse@gmail.com>
2020-01-28 23:00:23 +0900

    ofxOsc追加

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
+
+#define HOST "127.0.0.1"
+#define PORT 54414
 
 class ofApp : public ofBaseApp{
 
@@ -8,17 +12,7 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
 		
+		ofxOscReceiver receiver;
+		int oscMessage;
 };
